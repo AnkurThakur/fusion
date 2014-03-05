56f2713f20b77039473f80bcc2b70b3396bc51a2
Ankur Thakur <thakurboyz2007@gmail.com>
2014-03-05 14:23:13 +0530

    Uploaded 7.02.07 Files
    Installed PHP-Fusion 7.02.07.
    Now, proceeding to make changes in it, for implementing Permalinks and
    Rewrites.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// database settings
+$db_host = "localhost";
+$db_user = "fusion";
+$db_pass = "fusion";
+$db_name = "fusion";
+$db_prefix = "fusion_";
+define("DB_PREFIX", "fusion_");
+define("COOKIE_PREFIX", "fusion54634_");
+?>
